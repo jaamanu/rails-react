fe152d7e93cc3425d817234afd5ea542d38a3fe5
jaamanu <jaamanu@gmail.com>
2023-04-02 00:41:26 +0200

    react webpack app

---
+++ app/javascript/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Nav = () => (
+    <div>
+        <Link to="/greeting">greeting</Link>
+    </div>
+)
+export default Nav
